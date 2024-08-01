57ae32839ee374bc912015a1188798a0afd2496a
Astral <81131964+ravadon@users.noreply.github.com>
2024-08-01 15:33:19 +0300

    "Project" minor cleanup and update - Not tested

--- Source/ODBCConnectPlugin.Target.cs
+++ Source/ODBCConnectPlugin.Target.cs
@@ -8,8 +8,8 @@ public class ODBCConnectPluginTarget : TargetRules
 	public ODBCConnectPluginTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ODBCConnectPlugin");
 	}
 }
--- Source/ODBCConnectPluginEditor.Target.cs
+++ Source/ODBCConnectPluginEditor.Target.cs
@@ -8,7 +8,7 @@ public class ODBCConnectPluginEditorTarget : TargetRules
 	public ODBCConnectPluginEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
     }
 }
